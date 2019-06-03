2249cf37c77116873a965602315b02b480150d86
Anita <anita.martin5703@gmail.com>
2019-06-03 14:09:35 -0600

    Added in Fibonacci

--- src/edu/cnm/deepdive/Palindromes.java
+++ src/edu/cnm/deepdive/Palindromes.java
@@ -2,14 +2,16 @@ package edu.cnm.deepdive;
 
 public class Palindromes {
 
-  public static boolean isPalindrome(String s){
-    return isSimplePalindrome(s.replaceAll("\\W+", "").toLowerCase());
+  public static boolean isPalindrome(String s) {
+    return isSimplePalindrome(s.replaceAll("[\\W_]+", "").toLowerCase());
   }
 
-  public static boolean isSimplePalindrome(String s){
+  private static boolean isSimplePalindrome(String s) {
     return s.length() <= 1
-        || (s.charAt(0) == s.charAt(s.length() -1)
-        && isSimplePalindrome(s.substring(1, s.length() -1))
+        || (
+        s.charAt(0) == s.charAt(s.length() - 1)
+            && isSimplePalindrome(s.substring(1, s.length() - 1))
     );
   }
+
 }
